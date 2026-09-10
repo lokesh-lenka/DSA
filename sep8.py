d84f4dc5bf5ce5c1410446906a327b5c12677c56
Lokesh Lenka <lenkalokesh2003@gmail.com>
2026-09-10 11:21:44 +0530

    second commit

--- sep8.py
+++ sep8.py
@@ -17,7 +17,7 @@ print(f'count of students marks list is {marks}')
 
 BMI
 -----
-
+1)
 n_of_t_user_input = int(input("enter the value:"))
 for i in range(n_of_t_user_input):
     weight = float(input("Enter the weight in kgs:"))
@@ -38,7 +38,7 @@ for i in range(n_of_t_user_input):
     else:
         print("enter positive values")
 
-'''
+2)
 while True:
     weight = int(input("enter the weight in kgs:"))
     height = float(input("enter the height in metres:"))
@@ -58,6 +58,134 @@ while True:
             print("invalid input")
     except Exception as e:
         print(f"The Error is {e}")
+
+number list analyzer
+------------------------
+
+
+numbers = [20, 10, 30, 20, 40, 20]
+print(f'the original list is {numbers}')
+numbers.sort()
+numbers.reverse()
+print(numbers)
+num = int(input("Enter a number to search for: "))
+if num in numbers:
+    print(f'the count of this number is {numbers.count(num)}')
+    print(f'the index of this number is {numbers.index(num)}')
+    print(f'the largest value is {max(numbers)}')
+    print(f'the smallest value is {min(numbers)}')
+    print(f'the smallest value is {sum(numbers)}')
+else:
+    print("number not found")
+
+
+even and odd number separation
+---------------------------------
+
+
+nums = [10, 15, 20, 25, 30, 35]
+even = []
+odd = []
+for num in nums:
+    if num % 2 == 0:
+        even.append(num)
+    else:
+        odd.append(num)
+
+print("Even nums:", even)
+print("Odd nums:", odd)
+print("First three values:", nums[:3])
+print("Last three values:", nums[-3:])
+
+nums_backup = nums.copy()
+
+nums.clear()
+
+print("Original list using clear():", nums)
+print("Backup list:", nums_backup)
+
+
+unique name manager
+-----------------------
+
+
+names = ["Asha", "Rahul", "Asha","John","Rahul"]
+print(names)
+names = set(names)
+print(names)
+names.add("Meera")
+names.update(["Arun","Priya"])
+print(names)
+if "John" in names:
+    names.remove("John")
+print(names)
+names.discard("David")
+print(names)
+for name in names:
+    print(name)
+
+
+course student comparision
+-------------------------------
+
+'''
+
+python_students = {"Asha", "Rahul", "John", "Meera"}
+da_students = {"Rahul", "Meera", "Arun"}
+
+print("--- Union ---")
+for student in python_students | da_students:
+    print(student)
+
+print("\n--- Intersection ---")
+for student in python_students & da_students:
+    print(student)
+
+print("\n--- Symmetric Difference ---")
+for student in python_students ^ da_students:
+    print(student)
+
+print("\n--- Relationships ---")
+if da_students <= python_students:
+    print("DA is a subset of Python")
+else:
+    print("DA is NOT a subset of Python")
+
+if da_students >= python_students:
+    print("DA is a superset of Python")
+else:
+    print("DA is NOT a superset of Python")
+
+if da_students.isdisjoint(python_students):
+    print("Classes are disjoint")
+else:
+    print("Classes are NOT disjoint")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         
 
 
---
+++ sep9.py
@@ -0,0 +1,132 @@
+'''
+
+Python project --> POP / OOP --> DSA (Logic based --> pattern based --> plaform based)
+POP --> Dividing he entire code into blocks
+blocks --> procedures --> functions (def)
+Functions --> A resualble block of code (A block of statments which perform a specific task)
+
+Syntax:
+
+def < funcname>(parameters):
+	"""Doc String"""
+	statement(s).....
+	...........  #body of func
+	return values(s)...
+fname(args) # func call
+		
+
+
+#simple scnario to understand
+
+def add(a,b):
+	"""Addition function"""
+	c = a+ b
+	return c
+print(add(4,6)) #addition
+c,d = 'codegnan','python'
+print(add(c,d)) #concatenation
+e,f = map(str,input("enter the values").split(','))
+print(add(e,f))
+print(add([1,3,4],[4,6,7])) #merging
+#print(add(1,2,3,4)) #positional arguments fail
+
+
+
+#Variable length arguments --> *args We can pass any number of positional
+#arguments-->data will be stored in tuple...
+
+def sample(*a):
+    """Demo of Variable length arguments"""
+    print(a)
+    print(type(a)) #default it stores in tuple format
+sample()
+sample(2,3,4,5)
+sample('codegnan',[23,4],'poll',2+5j)
+
+marks = [20,15,25,18]
+sample(marks)
+sample(*marks)
+*a,b,c=12,'code','poll',23,4,9
+print(a)
+print(b)
+print(c)
+
+
+
+def add(*a):
+    """Perform addition for numeric values"""
+    print(a)
+    result = 0
+    for i in a:
+        #print(i)
+        #if type(i) in [int,float]:
+        if type(i) == int or type(i) == float:
+            result = result + i
+    return result
+print(add(2,3,4))
+print(add(2,'codegnan',3,4))
+
+
+#keyword arguments -->we can pass the name for the arguments
+def batch(name,age,place="Vizag"):
+#def batch(name="Saketh",age,place='Hyd'): #error ->non default always follows default arguments
+    """Keyword arguments usage"""
+    print(f'{name} is in {place} and age is {age} years')
+batch('Codegnan',1,'Vizag')
+batch(place='Vizag',name='Codegnan',age=1)
+#keyword arguments only needs name matching not order
+batch(name="Saketh",age=32)
+#default arguments can accept a value as default
+
+
+print(4,5)
+print(4,5,sep=':') #here keyword arguments is sep and we are changing
+#the default value for sep
+
+
+#keyword variable length arguments (**kwargs) -->Any number of
+#keyword arguments,data is stored in dictionary
+
+def batch(**a):
+    """Keyword variable length arguments usage"""
+    print(a)
+    print(type(a))
+batch()
+batch(name="akash",age=21,place="vizag",branch="CSE")
+
+data = {'name':['Akash','Praneeth'],
+        'place':['Vizag','Rajahmundry']}
+#batch(**data)
+data.update({'batch':'PFS-VSP-007'})
+batch(**data)
+
+#Task : Create a function with the usage of *args & **kwargs
+
+def fn(*a,**b):
+    ....
+    .....
+fn(*c,**d)
+
+'''
+def fn(*a, **b):
+    print("a =", a)  # tuple of positional arguments
+    print("b =", b)  # dictionary of keyword arguments
+
+
+# Data to pass
+c = [10, 20]
+d = {"x": 1, "y": 2}
+
+# Call the function by unpacking
+fn(*c, **d)
+
+
+
+
+
+
+
+
+
+
+
